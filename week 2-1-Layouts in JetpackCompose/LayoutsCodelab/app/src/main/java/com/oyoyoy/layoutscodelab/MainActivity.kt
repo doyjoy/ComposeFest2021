bda383d33a7bffe239994966ec30d743f1355c11
Joy <88281884+doyjoy@users.noreply.github.com>
2021-11-14 23:03:49 +0900

    211114 LayoutsCodelab Done
    4시간 40분 걸려서 완료!

--- week 2-1-Layouts in JetpackCompose/LayoutsCodelab/app/src/main/java/com/oyoyoy/layoutscodelab/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/LayoutsCodelab/app/src/main/java/com/oyoyoy/layoutscodelab/MainActivity.kt
@@ -10,20 +10,23 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.AlignmentLine
-import androidx.compose.ui.layout.FirstBaseline
-import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.layout.layout
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.*
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 import coil.compose.rememberImagePainter
 import com.oyoyoy.layoutscodelab.ui.theme.LayoutsCodelabTheme
 import kotlinx.coroutines.launch
@@ -47,17 +50,12 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
- /*
-@Composable
-fun Button(
-    modifier: Modifier = Modifier,
-    onClick : (() -> Unit)? = null,
-    content : @Composable () -> Unit
-){
-    Row {
+val topics = listOf(
+    "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
+    "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
+    "Religion", "Social sciences", "Technology", "TV", "Writing"
+)
 
-    }
-} */
 
 @Composable
 fun LayoutsCodelab(){
@@ -66,33 +64,258 @@ fun LayoutsCodelab(){
             TopAppBar(
                 title = {
                     Text(text = "LayoutsCodelab")
+                },
+                actions = {
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(Icons.Filled.Favorite, contentDescription = null)
+                    }
                 }
             )
         }
     ) {
-        innerPadding ->
-        BodyContent(modifier = Modifier
-            .padding(innerPadding)
-            .padding(8.dp))
+        innerPadding -> BodyContent(modifier = Modifier.padding(innerPadding))
+    }
+}
+@Composable
+fun BodyContent(modifier : Modifier = Modifier) {
+    Row(modifier = modifier
+        .background(color = Color.LightGray)
+        .padding(16.dp)
+        .size(200.dp)
+        .horizontalScroll(rememberScrollState()) ,
+        content = {  StaggeredGrid {
+            for (topic in topics) {
+                Chip(modifier = Modifier.padding(8.dp), text = topic)
+            }
+        } })
+    /*    Column(modifier = modifier) {
+        ImageList()
+    }
+    JoyOwnColumn(modifier.padding(8.dp)) {
+        Text("MyOwnColumn")
+        Text("places items")
+        Text("vertically.")
+        Text("We've done it by hand!")
+    }    */
+}
+
+@Composable
+fun TwoTexts(modifier : Modifier = Modifier, text1:String, text2:String){
+    Row (modifier = modifier.height(IntrinsicSize.Min)){
+        Text(
+            modifier = Modifier.weight(1f).padding(start = 4.dp).wrapContentWidth(Alignment.Start),
+            text = text1
+        )
+        Divider (color = Color.Black, modifier = Modifier.fillMaxHeight().width(1.dp))
+        Text(
+            modifier = Modifier.weight(1f).padding(end = 4.dp).wrapContentWidth(Alignment.End),
+            text = text2
+        )
+    }
+}
+@Preview
+@Composable
+fun TwoTextsPreview() {
+    LayoutsCodelabTheme {
+        Surface {
+            TwoTexts(text1 = "Hi", text2 = "Bye")
+        }
+    }
+}
+@Composable
+fun ConstraintLayoutContent(){
+    ConstraintLayout {
+        val (button1, button2, text) = createRefs()
+        Button (
+            onClick = {},
+            modifier = Modifier.constrainAs(button1){
+                top.linkTo(parent.top, margin = 16.dp)
+            }
+        ){
+            Text("Button 1")
+        }
+        Text("Text", Modifier.constrainAs(text){
+            top.linkTo(button1.bottom, margin = 16.dp)
+            centerAround(button1.end)
+        })
+
+        val barrier = createEndBarrier(button1, text)
+        Button (
+            onClick = {},
+            modifier = Modifier.constrainAs(button2){
+                top.linkTo(parent.top, margin = 16.dp)
+                start.linkTo(barrier)
+            }
+        ) {
+            Text("Button 2")
+        }
+
+    }
+    /*
+    BoxWithConstraints {
+        val constraints = if (maxWidth < maxHeight) {
+            decoupledConstraints(margin = 16.dp) // Portrait constraints
+        } else {
+            decoupledConstraints(margin = 32.dp) // Landscape constraints
+        }
+
+        ConstraintLayout(constraints) {
+            Button(
+                onClick = { /* Do something */ },
+                modifier = Modifier.layoutId("button")
+            ) {
+                Text("Button")
+            }
+
+            Text("Text", Modifier.layoutId("text"))
+        }
+    }*/
+}
+@Composable
+fun LargeConstraintLayout(){
+    ConstraintLayout {
+        val text = createRef()
+
+        val guideline = createGuidelineFromStart(fraction = 0.5f)
+        Text(
+            "This is very very very very very very very very very very long text."
+            ,Modifier.constrainAs(text){
+                linkTo(start = guideline, parent.end)
+                width = Dimension.preferredWrapContent
+            }
+        )
+    }
+}
+
+@Preview
+@Composable
+fun LargeConstraintLayoutPreview() {
+    LayoutsCodelabTheme {
+        LargeConstraintLayout()
+    }
+}
+
+@Preview
+@Composable
+fun ConstraintLayoutContentPreview() {
+    LayoutsCodelabTheme {
+        ConstraintLayoutContent()
+    }
+}
+@Composable
+private fun decoupledConstraints(margin: Dp): ConstraintSet {
+    return ConstraintSet {
+        val button = createRefFor("button")
+        val text = createRefFor("text")
+
+        constrain(button) {
+            top.linkTo(parent.top, margin)
+        }
+        constrain(text) {
+            top.linkTo(button.bottom, margin)
+        }
+    }
+}
+
+
+
+
+
+
+@Composable
+fun StaggeredGrid(
+    modifier: Modifier = Modifier,
+    rows : Int = 3,
+    content: @Composable () -> Unit
+){
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        val rowWidths = IntArray(rows) {0}
+        val rowHeights = IntArray(rows) {0}
+
+        val placeables = measurables.mapIndexed{
+            index, measurable ->
+            val placeable = measurable.measure(constraints)
+
+            val row = index % rows
+            rowWidths[row] += placeable.width
+            rowHeights[row] = Math.max(rowHeights[row], placeable.height)
+            placeable
+        }
+
+        val width = rowWidths.maxOrNull()?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth)) ?: constraints.minWidth
+        val height = rowHeights.sumOf { it }.coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+
+        val rowY = IntArray(rows) { 0 }
+        for (i in 1 until rows) {
+            rowY[i] = rowY[i-1] + rowHeights[i-1]
+        }
+
+        layout(width, height) {
+            val rowX = IntArray(rows) {0}
+            placeables.forEachIndexed {
+                index, placeable ->
+                val row = index % rows
+                placeable.placeRelative(
+                    x = rowX[row],
+                    y = rowY[row]
+                )
+                rowX[row] += placeable.width
+            }
+        }
     }
 }
 
 @Composable
-fun Modifier.firstBaselineToTop(firstBaselineToTop : Dp) = this.then(
-    layout{
-            measurable, constraints ->
-        val placeable = measurable.measure(constraints)
+fun Chip (modifier : Modifier = Modifier, text : String) {
+    Card (
+        modifier = modifier,
+        border = BorderStroke(color = Color.Black, width = Dp.Hairline),
+        shape = RoundedCornerShape(8.dp)
+    ){
+        Row (
+            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Box (
+                modifier = Modifier
+                    .size(16.dp)
+                    .background(color = MaterialTheme.colors.secondary)
+            )
+            Spacer(Modifier.width(4.dp))
+            Text(text = text)
 
-        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
-        val firstBaseLine = placeable[FirstBaseline]
-
-        val placeableY = firstBaselineToTop.roundToPx() -firstBaseLine
-        val height = placeable.height + placeableY
-        layout(placeable.width, height){
-            placeable.placeRelative(0, placeableY)
         }
     }
-)
+}
+
+/*
+
+@Preview
+@Composable
+fun LayoutsCodelabPreview() {
+    LayoutsCodelabTheme {
+        LayoutsCodelab()
+    }
+}
+*/
+
+
+
+
+
+
+/*
+@Preview
+@Composable
+fun ChipPreview() {
+    LayoutsCodelabTheme {
+        Chip(text = "Hi there")
+    }
+}
+*/
 
 
 @Composable
@@ -104,36 +327,21 @@ fun JoyOwnColumn(
         modifier = modifier,
         content = content
     ) {
-        measurables, constraints ->
+            measurables, constraints ->
 
         val placeables = measurables.map {
-            measurable ->
+                measurable ->
             measurable.measure(constraints)
         }
         var yPosition = 0
 
         layout(constraints.maxWidth, constraints.maxHeight) {
             placeables.forEach {
-                placeable ->
+                    placeable ->
                 placeable.placeRelative(x = 0, y = yPosition)
                 yPosition += placeable.height
             }
         }
-    }
-}
-
-@Composable
-fun BodyContent(modifier : Modifier = Modifier) {
-    /*
-    Column(modifier = modifier) {
-        ImageList()
-    }
-    */
-    JoyOwnColumn(modifier.padding(8.dp)) {
-        Text("MyOwnColumn")
-        Text("places items")
-        Text("vertically.")
-        Text("We've done it by hand!")
     }
 }
 
@@ -212,10 +420,20 @@ fun PhotographerCard(modifier: Modifier = Modifier) {
     }
 }
 
-@Preview
+
 @Composable
-fun LayoutsCodelabPreview() {
-    LayoutsCodelabTheme {
-        LayoutsCodelab()
+fun Modifier.firstBaselineToTop(firstBaselineToTop : Dp) = this.then(
+    layout{
+            measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseLine = placeable[FirstBaseline]
+
+        val placeableY = firstBaselineToTop.roundToPx() -firstBaseLine
+        val height = placeable.height + placeableY
+        layout(placeable.width, height){
+            placeable.placeRelative(0, placeableY)
+        }
     }
-}
+)
