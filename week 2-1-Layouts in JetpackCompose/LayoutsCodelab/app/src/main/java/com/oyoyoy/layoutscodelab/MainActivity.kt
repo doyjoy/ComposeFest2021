93ccdf38db8f3b3b932202b80cfdfa4a0b7b65f2
Joy <88281884+doyjoy@users.noreply.github.com>
2021-11-14 18:12:16 +0900

    211114 LayoutsCodelab 8. Complex custom layout 부터!

--- week 2-1-Layouts in JetpackCompose/LayoutsCodelab/app/src/main/java/com/oyoyoy/layoutscodelab/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/LayoutsCodelab/app/src/main/java/com/oyoyoy/layoutscodelab/MainActivity.kt
@@ -12,14 +12,21 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.oyoyoy.layoutscodelab.ui.theme.LayoutsCodelabTheme
+import kotlinx.coroutines.launch
 
 
 class MainActivity : ComponentActivity() {
@@ -40,7 +47,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
+ /*
 @Composable
 fun Button(
     modifier: Modifier = Modifier,
@@ -50,7 +57,7 @@ fun Button(
     Row {
 
     }
-}
+} */
 
 @Composable
 fun LayoutsCodelab(){
@@ -71,23 +78,98 @@ fun LayoutsCodelab(){
 }
 
 @Composable
+fun Modifier.firstBaselineToTop(firstBaselineToTop : Dp) = this.then(
+    layout{
+            measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseLine = placeable[FirstBaseline]
+
+        val placeableY = firstBaselineToTop.roundToPx() -firstBaseLine
+        val height = placeable.height + placeableY
+        layout(placeable.width, height){
+            placeable.placeRelative(0, placeableY)
+        }
+    }
+)
+
+
+@Composable
+fun JoyOwnColumn(
+    modifier: Modifier = Modifier,
+    content : @Composable () -> Unit
+){
+    Layout(
+        modifier = modifier,
+        content = content
+    ) {
+        measurables, constraints ->
+
+        val placeables = measurables.map {
+            measurable ->
+            measurable.measure(constraints)
+        }
+        var yPosition = 0
+
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            placeables.forEach {
+                placeable ->
+                placeable.placeRelative(x = 0, y = yPosition)
+                yPosition += placeable.height
+            }
+        }
+    }
+}
+
+@Composable
 fun BodyContent(modifier : Modifier = Modifier) {
+    /*
     Column(modifier = modifier) {
         ImageList()
-        Text(text = "Hi There!")
-        Text(text = "Thanks for going through the Layouts codelab")
+    }
+    */
+    JoyOwnColumn(modifier.padding(8.dp)) {
+        Text("MyOwnColumn")
+        Text("places items")
+        Text("vertically.")
+        Text("We've done it by hand!")
     }
 }
 
 @Composable
 fun ImageList (){
+    val listSize = 100
     val scrollState = rememberLazyListState()
-    LazyColumn(state = scrollState){
-        items(100){
-            imageListItem(index = it)
+    val coroutineScope = rememberCoroutineScope()
+    Column {
+        Row{
+            Button(
+                onClick = {
+                    coroutineScope.launch {
+                        scrollState.animateScrollToItem(0)
+                    }
+                }
+            ){
+                Text("Scroll to the top")
+            }
+            Button(
+                onClick = {
+                    coroutineScope.launch {
+                        scrollState.animateScrollToItem((listSize -1 ))
+                    }
+                }
+            ){
+                Text("Scroll to the end")
+            }
+        }
+
+        LazyColumn(state = scrollState){
+            items(listSize){
+                imageListItem(index = it)
+            }
         }
     }
-
 }
 
 @Composable
