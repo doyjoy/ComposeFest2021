6858a18069b17f3330060ebf1bc4110bcc3f1fcc
Joy <88281884+doyjoy@users.noreply.github.com>
2021-12-05 21:42:30 +0900

    211205 Navigate Codelab Done
    7 codelabs done!

--- week 3-3-Jetpack Compose Navigation/app/src/androidTest/java/com/example/compose/rally/RallyNavHostTest.kt
+++ week 3-3-Jetpack Compose Navigation/app/src/androidTest/java/com/example/compose/rally/RallyNavHostTest.kt
@@ -2,14 +2,8 @@ package com.example.compose.rally
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
-import androidx.compose.ui.test.performClick
-import androidx.compose.ui.test.performScrollTo
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
-import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -26,33 +20,10 @@ class RallyNavHostTest {
             RallyNavHost(navController = navController)
         }
     }
-
     @Test
     fun rallyNavHost() {
         composeTestRule
             .onNodeWithContentDescription("Overview Screen")
-            .assertIsDisplayed()
-    }
-
-    @Test
-    fun rallyNavHost_navigateToBills_viaUI(){
-        composeTestRule.onNodeWithContentDescription("All Bills").apply{
-            performScrollTo()
-            performClick()
-        }
-        val route = navController.currentBackStackEntry?.destination?.route
-        assertEquals(route,"Bills")
-    }
-
-    @Test
-    fun rallyNavHost_navigateToAllAccounts_callingNavigate(){
-        runBlocking {
-            withContext(Dispatchers.Main){
-                navController.navigate(RallyScreen.Accounts.name)
-            }
-        }
-        composeTestRule
-            .onNodeWithContentDescription("Account Screen")
             .assertIsDisplayed()
     }
 }
